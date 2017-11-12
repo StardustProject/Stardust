636f734a8db534a0dd1b966e7f383fcc0d7f93a3
xiong123 <1009224322@qq.com>
2017-11-12 12:27:47 +0800

    feat(文章): 文章界面

--- Stardust/app/src/main/java/org/swsd/stardust/view/fragment/ArticleFragment.java
+++ Stardust/app/src/main/java/org/swsd/stardust/view/fragment/ArticleFragment.java
@@ -21,6 +21,5 @@ public class ArticleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_article, null);
-
     }
 }
