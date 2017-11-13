2c5794c4ed7214ac85620b01afca23fb59d1e0d1
luojingzhao <1241138441@qq.com>
2017-11-13 23:22:53 +0800

    fix(ActivityCollector): 注释
    - 修改了ActivityCollector的不规范代码注释

--- Stardust/app/src/main/java/org/swsd/stardust/base/ActivityCollector.java
+++ Stardust/app/src/main/java/org/swsd/stardust/base/ActivityCollector.java
@@ -15,18 +15,19 @@ import java.util.List;
 public class ActivityCollector {
     private static List<Activity> activityList = new ArrayList<>();
 
-    /** 添加一个Activity到集合中*/
+    // 添加一个Activity到集合中
     public static void addActivity(Activity activity){
         activityList.add(activity);
     }
 
-    /** 从集合中删除一个Activity*/
+    //从集合中删除一个Activity
     public static void removeActivity(Activity activity){
         activityList.remove(activity);
     }
 
     /**获取Activity栈中的栈顶的Activity
-     * 需要注意的是，栈是先进后出，所以最上面的Activity是集合中的最后一个*/
+     * 需要注意的是，栈是先进后出，所以最上面的Activity是集合中的最后一个
+     */
     public static Activity getTopActivity(){
         if (activityList.isEmpty()){  //Activity栈为空
             return null;
