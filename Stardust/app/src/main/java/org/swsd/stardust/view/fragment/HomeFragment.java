b5c54b52a3aebb29aec6a5c3cfcb6f1ccedc1321
ZhaoxiZhang <897488058@qq.com>
2017-11-14 21:22:13 +0800

    feat(主页): 主页UI绘制

--- Stardust/app/src/main/java/org/swsd/stardust/view/fragment/HomeFragment.java
+++ Stardust/app/src/main/java/org/swsd/stardust/view/fragment/HomeFragment.java
@@ -22,7 +22,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-       return inflater.inflate(R.layout.fragment_tab_1, null);
+       return inflater.inflate(R.layout.fragment_home, null);
 
     }
 }
