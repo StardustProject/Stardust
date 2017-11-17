96b3eb0fef7c3092973b6b664bf4b17e27ecd129
ZhaoxiZhang <897488058@qq.com>
2017-11-17 10:40:41 +0800

    feat(主页): 实现光点跳转逻辑 - 添加Activity之间数据传递

--- Stardust/app/src/main/java/org/swsd/stardust/view/fragment/HomeFragment.java
+++ Stardust/app/src/main/java/org/swsd/stardust/view/fragment/HomeFragment.java
@@ -1,33 +1,25 @@
 package org.swsd.stardust.view.fragment;
 
 
-import android.app.DatePickerDialog;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.litepal.crud.DataSupport;
 import org.swsd.stardust.R;
 import org.swsd.stardust.model.bean.NoteBean;
 import org.swsd.stardust.presenter.HomePresenter;
 import org.swsd.stardust.presenter.IHomePresenter;
 import org.swsd.stardust.presenter.adapter.HomeAdapter;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 /**
  *    author     :  张昭锡
@@ -51,7 +43,6 @@ public class HomeFragment extends Fragment implements IHomeView,View.OnClickList
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_home,container,false);
 
-
         //初始化
         mHomePresenter = new HomePresenter(this);
 
@@ -72,7 +63,7 @@ public class HomeFragment extends Fragment implements IHomeView,View.OnClickList
 
         //设置瀑布流为4列
         StaggeredGridLayoutManager layoutManager = new
-                StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL);
+                StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL);
         mRvLightspot.setLayoutManager(layoutManager);
 
         //创建主页适配器
