0f8b28d9a9949e7455e27214deac0df674ada5e1
xiong123 <1009224322@qq.com>
2017-11-18 21:06:38 +0800

    feat(文章): 获取服务器文章
    - Get 服务器文章
    - 对需要提取内容的文章进行解析

--- Stardust/app/src/main/java/org/swsd/stardust/presenter/ArticlePresenter/PutArticle.java
+++ Stardust/app/src/main/java/org/swsd/stardust/presenter/ArticlePresenter/PutArticle.java
@@ -7,5 +7,50 @@ package org.swsd.stardust.presenter.ArticlePresenter;
  * version: 1.0
  */
 public class PutArticle {
-    // TODO: 2017/11/18
+    private String url;
+    private String publish_time;
+    private String author;
+    private String title;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPublish_time() {
+        return publish_time;
+    }
+
+    public void setPublish_time(String publish_time) {
+        this.publish_time = publish_time;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private String content;
 }
--- Stardust/app/src/main/java/org/swsd/stardust/view/fragment/ArticleFragment.java
+++ Stardust/app/src/main/java/org/swsd/stardust/view/fragment/ArticleFragment.java
@@ -11,21 +11,29 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.swsd.stardust.R;
 import org.swsd.stardust.model.bean.UserBean;
 import org.swsd.stardust.presenter.ArticlePresenter.Article;
 import org.swsd.stardust.presenter.ArticlePresenter.ArticleAdapter;
 import org.swsd.stardust.presenter.ArticlePresenter.JsoupWeiXin;
+import org.swsd.stardust.presenter.ArticlePresenter.PutArticle;
 import org.swsd.stardust.presenter.UserPresenter;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -35,14 +43,21 @@ import okhttp3.Response;
  *     version : 1.0
  */
 public class ArticleFragment extends Fragment {
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private static final String TAG = "熊立强：ArticleFragment";
     private RecyclerView recyclerView;
+    private static List<Article> mArticleList = new ArrayList<>();
+    List<String> articleUrls = new ArrayList<>();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_article,container,false);
         recyclerView = (RecyclerView) view.findViewById(R.id.article_recycler_view);
-        refreshRv();
+        getArticle();
+        while (true){
+            refreshRv();
+            if (mArticleList.size() == 10) break;
+        }
         return view;
 
     }
@@ -50,18 +65,17 @@ public class ArticleFragment extends Fragment {
     private void refreshRv(){
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
-        ArticleAdapter adapter = new ArticleAdapter(getTestAdapter());
+        ArticleAdapter adapter = new ArticleAdapter(mArticleList);
         recyclerView.setAdapter(adapter);
-        getArticle();
     }
     /**
      *  获得测试测试的适配器
      * @return List类型的文章列表
      */
-    List<Article> getTestAdapter(){
+    List<Article> getTestAdapter(List<String> urls){
         List<Article> list = new ArrayList<>();
-        for (int i = 0; i < 30 ; i++) {
-            String articleUrl = "https://mp.weixin.qq.com/s/zfRFqZmk-AVtZ9BdQkllPA";
+        for (String url: urls) {
+            String articleUrl = url;
             Article article = new Article();
             JsoupWeiXin.parseContent(articleUrl);
             article.setArticleTitle(JsoupWeiXin.title);
@@ -72,16 +86,6 @@ public class ArticleFragment extends Fragment {
             list.add(article);
         }
         return list;
-    }
-
-
-    // 使用Gson解析json格式的数据
-    private void parseJSONWithGSON(String jsonData){
-        Gson gson = new Gson();
-        List<Article> articleList = gson.fromJson(jsonData, new TypeToken<List<Article>>(){}.getType());
-        for(Article article : articleList){
-            Log.d(TAG, "name is  " + article.getArticleUrl());
-        }
     }
 
     /**
@@ -99,14 +103,39 @@ public class ArticleFragment extends Fragment {
                 Log.d(TAG, "userBean" + userBean.getUserId());
                 OkHttpClient client = new OkHttpClient();
                 Request request = new Request.Builder()
-                        .url("")
+                        .url("http://119.29.179.150:81/api/users/"+ userBean.getUserId()+"/articles?all_random=1")
+                        .addHeader("Authorization",userBean.getToken())
                         .build();
                 try {
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
                     Log.d(TAG, "run: Get response()" + responseData);
+                    JSONObject jsonObject = new JSONObject(responseData);
+                    if(jsonObject.getInt("error_code") == 200){
+                        Log.d(TAG, "error_code == 200");
+                        JSONArray jsonArray = new JSONArray(jsonObject.getString("articles"));
+                        Log.d(TAG, "run: JsonArray is " + jsonArray);
+                        for(int i = 0; i < jsonArray.length(); i++){
+                            JSONObject article = jsonArray.getJSONObject(i);
+                            String id = article.getString("id");
+                            String url = article.getString("url");
+                            String status = article.getString("need_dedication");
+                            Log.d(TAG, "run: id is " + id);
+                            Log.d(TAG, "run: url is "+url);
+                            Log.d(TAG, "run: status is " + status);
+
+                            if(status == "true"){
+                                // 需要发送的文章
+                                putArticle(url,id);
+                            }
+                            else{
+                                // 需要解析的文章
+                                parseArticle(url);
+                            }
+                        }
+                    }
                     // // TODO: 2017/11/18 解析json 获得url，根据url填充适配器
-                } catch (IOException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
 
@@ -117,7 +146,104 @@ public class ArticleFragment extends Fragment {
 
     }
 
-    private void putArticle(){
-        // TODO: 2017/11/18  上传url
+    /**
+     *  解析文章，不需要put形式
+     * @param url
+     */
+    private void parseArticle(final String url){
+            try {
+                Document doc = Jsoup.connect(url).get();
+                Elements elements = doc.select("h2[class=rich_media_title]");
+                Article ListItem = new Article();
+                Log.d("熊立强", "title" + elements.text());
+                ListItem.setArticleTitle(elements.text());
+                elements = doc.select("em[id=post-date]");
+                Log.d("熊立强", "publishTime:" + elements.text());
+                ListItem.setArtilePublishTime(elements.text());
+                elements = doc.select("span[class=rich_media_meta rich_media_meta_text rich_media_meta_nickname]");
+                Log.d("熊立强", "author" + elements.text());
+                ListItem.setArticleAuthor(elements.text());
+                Element content = doc.getElementById("js_content");
+                elements = content.getElementsByTag("p");
+                StringBuffer sb = new StringBuffer();
+                for (int j = 5; j < elements.size(); j++){
+                    sb.append(elements.get(j).text());
+                    //Log.d("熊立强", "run: " + elements.get(i).text());
+                }
+                Log.d("熊立强", "content all ：" + sb.toString());
+                ListItem.setArticleAbstract(sb.toString());
+                ListItem.setArticleUrl(url);
+                Log.d(TAG, "getArticle is " + ListItem.getArticleUrl());
+                Log.d(TAG, "getArticle is " + ListItem.getArticleTitle());
+                if(mArticleList.size() < 10){
+                    mArticleList.add(ListItem);
+                }
+                Log.d(TAG, "parseArticle: List size is " + mArticleList.size());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
+
+    private void putArticle(final String url,final String articleId){
+        // TODO: 2017/11/18  上传url
+            // 获取当前用户id
+            UserBean userBean;
+            UserPresenter userPresenter = new UserPresenter();
+            userBean = userPresenter.toGetUserInfo();
+            Log.d(TAG, "userBean" + userBean.getToken());
+            Log.d(TAG, "userBean" + userBean.getUserId());
+            // 解析url
+            try {
+                Document doc = Jsoup.connect(url).get();
+
+                PutArticle putArticle = new PutArticle();
+                Elements elements = doc.select("h2[class=rich_media_title]");
+                Log.d("熊立强", "title" + elements.text());
+                putArticle.setTitle(elements.text());
+                elements = doc.select("em[id=post-date]");
+                Log.d("熊立强", "publishTime:" + elements.text());
+                putArticle.setPublish_time(elements.text());
+                elements = doc.select("span[class=rich_media_meta rich_media_meta_text rich_media_meta_nickname]");
+                Log.d("熊立强", "author" + elements.text());
+                putArticle.setAuthor(elements.text());
+                Element content = doc.getElementById("js_content");
+                elements = content.getElementsByTag("p");
+                StringBuffer sb = new StringBuffer();
+                for (int j = 5; j < elements.size(); j++){
+                sb.append(elements.get(j).text());
+                //Log.d("熊立强", "run: " + elements.get(i).text());
+                }
+                Log.d("熊立强", "content all ：" + sb.toString());
+                putArticle.setContent(sb.toString());
+                putArticle.setUrl(url);
+                Gson gson = new Gson();
+                String json = gson.toJson(putArticle);
+                OkHttpClient client = new OkHttpClient();
+                RequestBody requestBody = RequestBody.create(JSON,json);
+                Request request = new Request.Builder()
+                        .url("http://119.29.179.150:81/api/users/"+ userBean.getUserId()+"/articles/" + articleId)
+                        .addHeader("Content-Type","application/json")
+                        .addHeader("Authorization",userBean.getToken())
+                        .put(requestBody)
+                        .build();
+                Response response = client.newCall(request).execute();
+                Log.d(TAG, "putResponse +" + response.body().string());
+                Article temp  = new Article();
+                temp.setArticleAbstract(putArticle.getContent());
+                temp.setArticleTitle(putArticle.getTitle());
+                temp.setArticleUrl(url);
+                temp.setArticleAuthor(putArticle.getAuthor());
+                temp.setArtilePublishTime(putArticle.getPublish_time());
+                if(mArticleList.size() < 10){
+                    mArticleList.add(temp);
+                }
+                Log.d(TAG, "List size " + mArticleList.size());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+    }
+
+
+
 }
